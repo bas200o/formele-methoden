ddfc9afd41472c5b1fcd241ef574d47f980678ef
CinqueIzumi <nofeednolife@gmail.com>
2021-11-20 16:03:27 +0100

    [Added] Added the example automata classes

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import com.company.automata.Automata;
+import com.company.automata.TestAutomata;
 import com.company.regex.TestRegExp;
 
 public class Main {
 
-        public static void main(String[] args) {
-            TestRegExp regExp = new TestRegExp();
-            regExp.testLanguage();
-        }
+    public static void main(String[] args) {
+//        TestRegExp regExp = new TestRegExp();
+//        regExp.testLanguage();
+        Automata first = TestAutomata.getExampleSlide8Lesson2();
+        Automata second = TestAutomata.getExampleSlide14Lesson2();
+
+        first.printTransitions();
+        second.printTransitions();
     }
+}
