8c18e106b3ac28d562f70dbe421d00a6aed33780
CinqueIzumi <ba.peereboom@gmail.com>
2022-01-21 12:23:29 +0100

    [Added] Extra tests

--- formele_methoden/Program.cs
+++ formele_methoden/Program.cs
@@ -5,9 +5,7 @@ namespace Formele_Methoden
 {
     class Program
     {
-        //TODO: RegEx -> NDFA
         //TODO: Check recursive function in ndfa -> dfa
-        //TODO: Implement reverse of dfa
         //TODO: Implement minimalisation -- toDfa(reverse(toDfa(reverse(dfa))))
         //TODO: Check for multiple start states
         //TODO: Clean up / work out examples and make proper demo class
@@ -15,10 +13,81 @@ namespace Formele_Methoden
         {
             //ndfaDemoOne();
             //ndfaDemoTwo();
-            //ndfaDemoEpsilonSimple();
+            ndfaDemoEpsilonSimple();
             //ndfaDemoEpsilonTwo();
 
-            dfaReverseExampleTwo();
+            //dfaMinimalize();
+            //testing();
+        }
+
+        private static void testing()
+        {
+            Ndfa testNdfa = new Ndfa();
+            testNdfa.addTransition(new CustomTransition("q0", "q0", "a"));
+            testNdfa.addTransition(new CustomTransition("q0", "q5", "a"));
+            testNdfa.addTransition(new CustomTransition("q0", "q2", "a"));
+
+            testNdfa.addTransition(new CustomTransition("q1", "q1", "b"));
+            testNdfa.addTransition(new CustomTransition("q1", "q0", "b"));
+            testNdfa.addTransition(new CustomTransition("q1", "q3", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q2", "q1", "a"));
+
+            testNdfa.addTransition(new CustomTransition("q3", "q4", "b"));
+            testNdfa.addTransition(new CustomTransition("q3", "q5", "b"));
+            testNdfa.addTransition(new CustomTransition("q3", "q2", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q4", "q3", "a"));
+
+            testNdfa.addTransition(new CustomTransition("q5", "q4", "a"));
+
+            testNdfa.addTransition(new CustomTransition("q6", "q4", "ε"));
+            testNdfa.addTransition(new CustomTransition("q6", "q2", "ε"));
+
+            testNdfa.markStartState("q6");
+            testNdfa.markEndState("q0");
+
+            testNdfa.drawGraph("ndfa", "../../../graphs/testing.dot");
+
+            Dfa testDfa = new Dfa(testNdfa);
+            testDfa.drawGraph("dfa", "../../../graphs/testing_dfa.dot");
+        }
+
+        private static void dfaMinimalize()
+        {
+            Ndfa testNdfa = new Ndfa();
+            testNdfa.addTransition(new CustomTransition("q0", "q0", "a"));
+            testNdfa.addTransition(new CustomTransition("q0", "q1", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q1", "q2", "a"));
+            testNdfa.addTransition(new CustomTransition("q1", "q1", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q2", "q0", "a"));
+            testNdfa.addTransition(new CustomTransition("q2", "q3", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q3", "q4", "a"));
+            testNdfa.addTransition(new CustomTransition("q3", "q1", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q4", "q5", "a"));
+            testNdfa.addTransition(new CustomTransition("q4", "q3", "b"));
+
+            testNdfa.addTransition(new CustomTransition("q5", "q0", "a"));
+            testNdfa.addTransition(new CustomTransition("q5", "q3", "b"));
+
+            testNdfa.markStartState("q0");
+            testNdfa.markEndState("q4");
+            testNdfa.markEndState("q2");
+
+            testNdfa.drawGraph("ndfa", "../../../graphs/graph_1.dot");
+
+            Dfa testDfa = new Dfa(testNdfa);
+            testDfa.drawGraph("dfa", "../../../graphs/graph_2.dot");
+
+            Ndfa rerversedNdfaOne = testDfa.getReverse();
+            rerversedNdfaOne.drawGraph("reversed_ndfa_1", "../../../graphs/graph_3.dot");
+
+            Dfa reversedDfaOne = new Dfa(rerversedNdfaOne);
+            reversedDfaOne.drawGraph("reversed_dfa_1", "../../../graphs/graph_4.dot");
         }
 
         private static void dfaReverseExampleOne()
