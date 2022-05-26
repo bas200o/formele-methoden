89f6e994b72cd7af0028ae9b344fad2f87e694e7
Cinque Izumi <ba.peereboom@gmail.com>
2022-05-26 13:34:47 +0200

    Added Regex demo (generate languages)

--- formele_methoden/Program.cs
+++ formele_methoden/Program.cs
@@ -7,204 +7,27 @@ namespace Formele_Methoden
     {
         static void Main(string[] args)
         {
-            // Regex demo
-            regexDemoOne();
-
-            // Convert a NDFA to a DFA
-            ndfaToDfaExampleOne();
-            ndfaToDfaExampleTwo();
-
-            // Convert a DFA to a NDFA (reverse the DFA)
-            dfaReverseExampleOne();
-
-            // Minimize a dfa -- can be done using the following: toDfa(reverse(toDfa(reverse(dfa))))
-            dfaMinimalizeExampleOne();
+            regexDemo();
         }
 
-        /// <summary>
-        /// A function which can be used to demonstrate the RegEx functionality within this application
-        /// </summary>
-        private static void regexDemoOne()
+        static private void regexDemo()
         {
-            Regex r = new Regex("(a|b)+(z)*(a|b)");
-            Ndfa ndfa = r.re2nfa();
-            ndfa.drawGraph("regexNDFA", "../../../graphs/regex_2_ndfa.dot");
-        }
+            Regex a = new Regex("a");
+            Regex b = new Regex("b");
 
-        /// <summary>
-        /// A function which can be used to demonstrate the minimalization functionality within this application
-        /// </summary>
-        private static void dfaMinimalizeExampleOne()
-        {
-            /*
-                Due to the non-implemented renaming system within the DFA, the minimizing of the DFA can not be done in one go
-                When done in a single line, the dfa will struggle with proper state transitions, because of those not having been renamed to simpler names after the first toDfa(reverse(dfa))
-                
-                We will show a written example of this during the assessment, as we can specifically point out where it would go wrong.
+            Regex all = (a.or(b)).star();
 
-                To display that the minimazation does work, however, we will demonstrate the steps seperately.
+            Regex aab = new Regex("aab");
+            Regex aa = new Regex("aa");
+            
+            Regex expression1 = aab.or(aa); // (aab|aa)
+            Regex expression2 = expression1.plus(); // (aab|aa)+
+            Regex expression3 = expression2.dot(all); // (aab|aa)+(a|b)*
 
-                This means that this specific demo works in the following way:
-                    1. Initialize the DFA which should be minimalized (this DFA will be written to the following file: original_dfa.dot)
-                    2. Reverse the DFA, creating a NDFA (this NDFA will be written to the following file: first_reversed_ndfa.dot)
-                    3. Create a new DFA, based on the above-mentioned NDFA (this DFA will be written to the following file: first_reversed_dfa.dot)
-                
-                    4. Re-create the DFA result from the program programmatically, but manually rename the states to simpler examples, to ensure that the program can handle the state transitions properly.
-                        The DFA, which we will re-create using code, is exactly the same as the DFA result, which can be found in first_reversed_dfa.dot
-                    5. Reverse the new DFA, creating a NDFA (this NDFA will be written to the following file: second_reversed_ndfa.dot
-                    6. Convert the latest NDFA to a DFA, which will be written to the following file: minimalized_dfa.dot
-             */
-
-            // Step 1
-            Ndfa testNdfa = new Ndfa();
-            testNdfa.addTransition(new CustomTransition("q0", "q0", "a"));
-            testNdfa.addTransition(new CustomTransition("q0", "q1", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q1", "q2", "a"));
-            testNdfa.addTransition(new CustomTransition("q1", "q1", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q2", "q0", "a"));
-            testNdfa.addTransition(new CustomTransition("q2", "q3", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q3", "q4", "a"));
-            testNdfa.addTransition(new CustomTransition("q3", "q1", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q4", "q5", "a"));
-            testNdfa.addTransition(new CustomTransition("q4", "q3", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q5", "q0", "a"));
-            testNdfa.addTransition(new CustomTransition("q5", "q3", "b"));
-
-            testNdfa.markStartState("q0");
-            testNdfa.markEndState("q4");
-            testNdfa.markEndState("q2");
-
-            testNdfa.drawGraph("ndfa", "../../../graphs/debuggraph.dot");
-
-            Dfa testDfa = new Dfa(testNdfa);
-            testDfa.drawGraph("dfa", "../../../graphs/original_dfa.dot");
-
-            // Step 2
-            Ndfa rerversedNdfaOne = testDfa.getReverse();
-            rerversedNdfaOne.drawGraph("reversed_ndfa_1", "../../../graphs/first_reversed_ndfa.dot");
-
-            // Step 3
-            Dfa reversedDfaOne = new Dfa(rerversedNdfaOne);
-            reversedDfaOne.drawGraph("reversed_dfa_1", "../../../graphs/first_reversed_dfa.dot");
-
-            // Step 4
-            Ndfa newTestNdfa = new Ndfa();
-            newTestNdfa.addTransition(new CustomTransition("q0", "q1", "a"));
-
-            newTestNdfa.addTransition(new CustomTransition("q1", "q2", "b"));
-
-            newTestNdfa.addTransition(new CustomTransition("q2", "q2", "a"));
-            newTestNdfa.addTransition(new CustomTransition("q2", "q2", "b"));
-
-            newTestNdfa.markStartState("q0");
-            newTestNdfa.markEndState("q2");
-
-            Dfa newTestDfa = new Dfa(newTestNdfa);
-
-            // Step 5
-            Ndfa reversedNdfaTwo = newTestDfa.getReverse();
-            reversedNdfaTwo.drawGraph("reversed_ndfa_2", "../../../graphs/second_reversed_ndfa.dot"); 
-
-            // Step 6
-            Dfa dfaMinimalized = new Dfa(reversedNdfaTwo);
-            dfaMinimalized.drawGraph("reversed_ndfa_2", "../../../graphs/minimalized_dfa.dot");
-        }
-
-        /// <summary>
-        /// A function which can be used to demonstrate the reverse functionality within this application
-        /// </summary>
-        private static void dfaReverseExampleOne()
-        {
-            Ndfa testNdfa = new Ndfa();
-            testNdfa.addTransition(new CustomTransition("q0", "q1", "a"));
-            testNdfa.addTransition(new CustomTransition("q0", "q2", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q1", "q1", "a"));
-            testNdfa.addTransition(new CustomTransition("q1", "q1", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q2", "q2", "a"));
-            testNdfa.addTransition(new CustomTransition("q2", "q2", "b"));
-
-            testNdfa.markStartState("q0");
-            testNdfa.markEndState("q1");
-
-            Dfa testDfa = new Dfa(testNdfa);
-            testDfa.drawGraph("graph_2", "../../../graphs/debuggraph.dot");
-
-            Ndfa reversedNdfa = testDfa.getReverse();
-            reversedNdfa.drawGraph("graph_3", "../../../graphs/dfa_reversed_one.dot");
-        }
-
-        /// <summary>
-        /// A function which can be used to demonstrate the first ndfa to dfa functionality within this application
-        /// </summary>
-        private static void ndfaToDfaExampleOne()
-        {
-            Ndfa testNdfa = new Ndfa();
-            testNdfa.addTransition(new CustomTransition("q1", "q4", "a"));
-            testNdfa.addTransition(new CustomTransition("q1", "q2", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q2", "q4", "a"));
-            testNdfa.addTransition(new CustomTransition("q2", "q1", "b"));
-            testNdfa.addTransition(new CustomTransition("q2", "q3", "b"));
-            testNdfa.addTransition(new CustomTransition("q2", "q3", "ε"));
-
-            testNdfa.addTransition(new CustomTransition("q3", "q5", "a"));
-            testNdfa.addTransition(new CustomTransition("q3", "q5", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q4", "q2", "ε"));
-            testNdfa.addTransition(new CustomTransition("q4", "q3", "a"));
-
-            testNdfa.addTransition(new CustomTransition("q5", "q4", "a"));
-            testNdfa.addTransition(new CustomTransition("q5", "q1", "b"));
-
-            testNdfa.markStartState("q1");
-            testNdfa.markEndState("q4");
-
-            //testNdfa.printTransitions();
-            testNdfa.drawGraph("graph_1", "../../../graphs/debuggraph.dot");
-
-
-            Dfa testDfa = new Dfa(testNdfa);
-            testDfa.drawGraph("graph_2", "../../../graphs/ndfa_2_dfa_one.dot");
-        }
-
-        /// <summary>
-        /// A function which can be used to demonstrate the second ndfa to dfa functionality within this application
-        /// </summary>
-        private static void ndfaToDfaExampleTwo()
-        {
-            Ndfa testNdfa = new Ndfa();
-            testNdfa.addTransition(new CustomTransition("q1", "q2", "a"));
-            testNdfa.addTransition(new CustomTransition("q1", "q3", "a"));
-            testNdfa.addTransition(new CustomTransition("q1", "q4", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q2", "q3", "a"));
-            testNdfa.addTransition(new CustomTransition("q2", "q3", "ε"));
-            testNdfa.addTransition(new CustomTransition("q2", "q1", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q3", "q3", "a"));
-            testNdfa.addTransition(new CustomTransition("q3", "q4", "ε"));
-            testNdfa.addTransition(new CustomTransition("q3", "q5", "b"));
-
-            testNdfa.addTransition(new CustomTransition("q4", "q5", "a"));
-
-            testNdfa.addTransition(new CustomTransition("q5", "q4", "a"));
-
-            testNdfa.markStartState("q1");
-            testNdfa.markEndState("q5");
-
-            //testNdfa.printTransitions();
-            testNdfa.drawGraph("graph_1", "../../../graphs/debuggraph.dot");
-
-
-            Dfa testDfa = new Dfa(testNdfa);
-            testDfa.drawGraph("graph_2", "../../../graphs/ndfa_2_dfa_two.dot");
+            Console.WriteLine("All possible combinations of (a|b)*\n" + all.getLanguageString(5));
+            Console.WriteLine("All possible combinations of (aab|aa)\n" + expression1.getLanguageString(5) + "\n\n");
+            Console.WriteLine("All possible combinations of (aab|aa)+\n" + expression2.getLanguageString(5) + "\n\n");
+            Console.WriteLine("All possible combinations of (aab|aa)+(a|b)*\n" + expression3.getLanguageString(5) + "\n\n");
         }
     }
 }
--- formele_methoden/Regex.cs
+++ formele_methoden/Regex.cs
@@ -1,189 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace formele_methoden
 {
     public class Regex
     {
-        private string _regex { get; set; }
-        public String startNode = null;
-        public String endNode = null;
-        public String selectedNode = null;
-        public List<String> connect2end = new List<string>();
-        public Dictionary<int, string> endNodes = new Dictionary<int, string>();
-        public List<Bridge> bridges = new List<Bridge>();
-        public String firstnode = null;
-        public String finalNode = null;
+        /// <summary>
+        /// An enum class which holds all the supported operators within the Regex class
+        /// </summary>
+        public enum SupportedOperators{ PLUS, STAR, OR, DOT, ONE }
 
-        /**
-        Bridge stuct serves to Link 2 nodes together.
-        Comparable to the CustomTransition class.
-        */
-        public struct Bridge
+        /// <summary>
+        /// The current operator within the regex
+        /// </summary>
+        private SupportedOperators currentOperator;
+
+        /// <summary>
+        /// The left and right part of the regex
+        /// </summary>
+        private Regex leftRegex= null;
+        private Regex rightRegex = null;
+
+        /// <summary>
+        /// The current regex string
+        /// </summary>
+        private string currentRegex;
+
+        /// <summary>
+        /// A method to get/set the current operator outside of this class
+        /// </summary>
+        public SupportedOperators CurrentOperator { get => currentOperator; set => currentOperator = value; }
+
+        /// <summary>
+        /// A method to get/set the right/left part of the regex
+        /// </summary>
+        public Regex RightRegex { get => rightRegex; set => rightRegex = value; }
+        public Regex LeftRegex { get => leftRegex; set => leftRegex = value; }
+
+        /// <summary>
+        /// A method to get/set the current regex
+        /// </summary>
+        public String CurrentRegex { get => currentRegex; set => currentRegex = value; }
+
+        /// <summary>
+        /// Constructor of the regex class
+        /// </summary>
+        /// <param name="givenRegex">The given refex which should be initialized</param>
+        public Regex(string givenRegex)
         {
-            public string startnode { get; }
-            public string endnode { get; }
-            public string key { get; }
-
-            public Bridge(string s, string e, string k)
-            {
-                startnode = s;
-                endnode = e;
-                key = k;
-            }
-        }
-
-        // Default constructor
-        public Regex()
-        {
-            this._regex = "(a|b)*";
-        }
-
-        // Constructer with Custom regex string
-        public Regex(string reg)
-        {
-            this._regex = reg;
-        }
-
-        // Calls the class variable for regex
-        public Ndfa re2nfa()
-        {
-            return re2nfa(this._regex);
+            currentRegex = givenRegex;
+            currentOperator = SupportedOperators.ONE;
         }
 
         /// <summary>
-        /// Converts regex string to a NDFA object.
-        /// supports all lowercase letter alphabet
+        /// Constructor of the regex class
         /// </summary>
-        /// <param name="regex">Regex string like (a|b)* supported opperators are | + *</param>
-        public Ndfa re2nfa(string regex)
+        public Regex()
         {
-            // Adds and 1 as end flag for the regex
-            regex = regex + "1";
-            Ndfa ndfa = new Ndfa();
+            currentRegex = "";
+            currentOperator = SupportedOperators.ONE;
+        }
 
-            // Main loop for converting regex to NDFA
-            for (int i = 0; i < regex.Length; i++)
+        /// <summary>
+        /// A method which adds a plus operator to the regex
+        /// </summary>
+        /// <returns>A new regex, a combination of the old regex and the new plus operator</returns>
+        public Regex plus()
+        {
+            Regex toReturn = new Regex();
+            toReturn.CurrentOperator = SupportedOperators.PLUS;
+
+            // Initialize the old regex within thhe new regex
+            toReturn.LeftRegex = this;
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// A method which adds a star operator to the regex
+        /// </summary>
+        /// <returns>A new regex, a combination of the old regex and the new star operator</returns>
+        public Regex star()
+        {
+            Regex toReturn = new Regex();
+            toReturn.CurrentOperator = SupportedOperators.STAR;
+
+            // Initialize the old regex within thhe new regex
+            toReturn.LeftRegex = this;
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// A method which can be used to add the 'or' operator to a regex
+        /// </summary>
+        /// <param name="givenRegex">The second part of the 'or' operator</param>
+        /// <returns>A new regex, which combines the current and the new regex in an 'or' operation</returns>
+        public Regex or(Regex givenRegex)
+        {
+            Regex toReturn = new Regex();
+            toReturn.CurrentOperator = SupportedOperators.OR;
+
+            // Initialize the left side of the 'or' operator (current regex)
+            toReturn.LeftRegex = this;
+
+            // Initialize the right side of the 'or' operator (given regex)
+            toReturn.RightRegex = givenRegex;
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// A method which can be used to add the 'dot' operator to a regex
+        /// </summary>
+        /// <param name="givenRegex">The second part of the 'dot' operator</param>
+        /// <returns>A new regex, which combines the current and the new regex in a 'dot' operation</returns>
+        public Regex dot(Regex givenRegex)
+        {
+            Regex toReturn = new Regex();
+            toReturn.CurrentOperator = SupportedOperators.DOT;
+
+            // Initialize the left side of the 'dot' operator (current regex)
+            toReturn.LeftRegex = this;
+
+            // Initialize the right side of the 'dot' operator (given regex)
+            toReturn.RightRegex = givenRegex;
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// A custom method, which allows for adding multiple items to a sorted set
+        /// </summary>
+        /// <param name="original">The original sorted set</param>
+        /// <param name="toAdd">The sorted set, which should be added to the original</param>
+        /// <returns>A sorted set containing the values of both the original and the new data</returns>
+        private SortedSet<string> addAll(SortedSet<string> original, SortedSet<string> toAdd)
+        {
+            foreach (string s in toAdd)
             {
-                char c = regex[i];
+                original.Add(s);
+            }
 
-                // Checks if a new alphabet has started
-                if (c == '(')
-                {
-                    startNode = "q" + i;
-                    selectedNode = startNode;
-                    if (endNode != null)
+            return original;
+        }
+
+        /// <summary>
+        /// A method which generates all acceptable combinations of a language in a sorted set
+        /// </summary>
+        /// <param name="maxSteps">The maximum amount of steps allowed</param>
+        /// <returns>A sorted set, which contains all the accepted languages</returns>
+        public SortedSet<string> getLanguage(int maxSteps)
+        {
+            SortedSet<string> toReturn = new SortedSet<string>(new SortByLength());
+            SortedSet<string> emptyLanguage = new SortedSet<string>(new SortByLength());
+
+            SortedSet<string> languageLeft, languageRight;
+
+            switch(this.currentOperator)
+            {
+                default:
+                    Console.WriteLine("This operator is not supported.");
+                    break;
+
+                case SupportedOperators.ONE:
+                    // Add the regex to the return value
+                    toReturn.Add(currentRegex);
+                    break;
+
+                case SupportedOperators.OR:
+                    // Initialize the left language
+                    if(leftRegex == null)
                     {
-                        link(endNode, startNode, "");
+                        languageLeft = emptyLanguage;
+                    } else
+                    {
+                        languageLeft = leftRegex.getLanguage(maxSteps - 1);
+                    }
+
+                    // Initialize the right language
+                    if(rightRegex == null)
+                    {
+                        languageRight = emptyLanguage;
+                    } else
+                    {
+                        languageRight = rightRegex.getLanguage(maxSteps - 1);
+                    }
+
+                    addAll(toReturn, languageLeft);
+                    addAll(toReturn, languageRight);
+                    break;
+
+                case SupportedOperators.DOT:
+                    // Initialize the left language
+                    if (leftRegex == null)
+                    {
+                        languageLeft = emptyLanguage;
                     }
                     else
                     {
-                        firstnode = startNode;
+                        languageLeft = leftRegex.getLanguage(maxSteps - 1);
                     }
-                    continue;
-                }
 
-                // checks if alphabet has ended
-                if (c == ')')
-                {
-                    endNode = "q" + i;
-                    link(selectedNode, endNode, "");
-                    connectEnds(connect2end, endNode);
-                    connect2end = new List<string>();
-                    selectedNode = endNode;
-                    endNodes.Add(i, endNode);
-                    continue;
-                }
-
-                // Makes 2 links from the start of the alphabet to the end
-                if (c == '*')
-                {
-                    if (startNode != null && endNode != null)
+                    // Initialize the right language
+                    if (rightRegex == null)
                     {
-                        link(startNode, endNode, "");
-                        link(endNode, startNode, "");
+                        languageRight = emptyLanguage;
                     }
-                }
-
-                // Makes a link from the back to the start of the alphabet
-                if (c == '+')
-                {
-                    if (startNode != null && endNode != null)
+                    else
                     {
-                        link(endNode, startNode, "");
+                        languageRight = rightRegex.getLanguage(maxSteps - 1);
                     }
-                }
 
-                // Creates the alphabet nodes and links staring node to it
-                if (Char.IsLower(c))
-                {
-                    List<char> chars = new List<char>();
-
-                    chars.Add(c);
-
-                    while (true)
+                    // Combine the left & right string(s)
+                    foreach(string stringLeft in languageLeft)
                     {
-                        char t = regex[i + 1];
-                        if (Char.IsLower(t))
+                        foreach(string stringRight in languageRight)
                         {
-                            chars.Add(t);
-                            i++;
-                        }
-                        else
-                        {
-                            String n = "q" + i;                            
-                            link(selectedNode, n, new string(chars.ToArray()));
-
-                            if (t != '|')
-                                selectedNode = n;
-                            else
-                                connect2end.Add(n);
-
-                            break;
+                            toReturn.Add(stringLeft + stringRight);
                         }
                     }
-                }
+                    break;
 
-                // Marks the end of the regex sting
-                if (c == '1')
-                {
-                    String n = "q" + i;
-                    link(selectedNode, n, "");
-                    this.finalNode = n;
-                }
+                case SupportedOperators.STAR:
+                case SupportedOperators.PLUS:
+                    // Initialize the left language
+                    if (leftRegex == null)
+                    {
+                        languageLeft = emptyLanguage;
+                    }
+                    else
+                    {
+                        languageLeft = leftRegex.getLanguage(maxSteps - 1);
+                    }
+                    addAll(toReturn, languageLeft);
+
+                    // Loop through all the remaining steps left
+                    for (int i = 0; i < maxSteps; i++)
+                    {
+                        // Add all possible new combinations
+                        HashSet<string> languageTemp = new HashSet<string>(toReturn);
+                        foreach (string s1 in languageLeft)
+                        {
+                            foreach (String s2 in languageTemp)
+                            {
+                                toReturn.Add(s1 + s2);
+                            }
+                        }
+                    }
+
+                    // If the operator is a star, no entry is valid too
+                    if(this.currentOperator == SupportedOperators.STAR)
+                    {
+                        toReturn.Add("");
+                    }
+                    break;
             }
 
-            // Converts the bridge object to Customtransition
-            foreach (Bridge b in bridges)
-            {
-                ndfa.addTransition(new CustomTransition(b.startnode, b.endnode, b.key));
-            }
-            ndfa.markStartState(this.firstnode);
-            ndfa.markEndState(this.finalNode);
-
-
-            return ndfa;
+            return toReturn;
         }
 
-
-        // Links 2 nodes with a lable for graphviz
-        public void link(String s, String e, String text)
+        /// <summary>
+        /// A method which generates all acceptable combinations of a language in a string
+        /// </summary>
+        /// <param name="maxSteps">The maximum of steps allowed</param>
+        /// <returns>A string, which contains all the accepted languages</returns>
+        public string getLanguageString(int maxSteps)
         {
-            if (text.Equals(""))
+            string toReturn = "";
+
+            foreach(string s in getLanguage(maxSteps))
             {
-                text = "ε";
+                toReturn += s;
+                toReturn += "\n";
             }
-            this.bridges.Add(new Bridge(s, e, text));
+
+            return toReturn;
         }
+    }
 
-        // Links all nodes with alphabet to the ending node
-        public void connectEnds(List<string> nodes, String end)
+    /// <summary>
+    /// A custom comparer, which allows sorted sets to be sorted, based on string length before string characters
+    /// </summary>
+    class SortByLength : IComparer<string>
+    {
+        public int Compare(string x, string y)
         {
-            if (!nodes.Any())
-            {
-                return;
-            }
+            // Check for potential null values
+            if (x == null) return y == null ? 0 : 1;
+            if (y == null) return -1;
 
-            foreach (string n in nodes)
+            if(x.Length == y.Length)
             {
-                link(n, end, "");
+                return x.CompareTo(y);
+            } 
+            else
+            {
+                return x.Length - y.Length;
             }
         }
     }
